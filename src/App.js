03b579e5c472b113c92255ef594c5a8e5d972d1a
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-12 11:40:24 +0200

    adding tailwind css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-
+import DocumentIcon from "./svg/Document";
+import CallIcon from "./svg/Call";
 function App() {
-  return (
-    <div>
-      <h1>Welcome to the frontend</h1>
-    </div>
-  );
+  return <div className="dark"></div>;
 }
 
 export default App;

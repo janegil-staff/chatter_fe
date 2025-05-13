61cfea0046ce3057eac0e2bb3eb060cc441f087c
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-13 13:55:26 +0200

    Setup react router dom, Use Redux persist to rehydrate only certain values

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import DocumentIcon from "./svg/Document";
-import CallIcon from "./svg/Call";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { useSelector } from "react-redux";
+
 function App() {
-  return <div className="dark"></div>;
+  const { user } = useSelector((state) => state.user);
+  console.log(user);
+  return (
+    <div className="dark">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
